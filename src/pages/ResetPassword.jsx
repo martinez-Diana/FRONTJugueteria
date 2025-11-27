9740457181b6ee9faaa6ff1bfb6817552c952bc1
Diana <Diana@email.com>
2025-11-27 10:50:36 -0600

    correccion vercel

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_URL || "https://back-jugueteria-andqdrj3h-20230105-8787s-projects.vercel.app";
 
 function ResetPassword() {
   const [searchParams] = useSearchParams();
