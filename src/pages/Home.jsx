acecff4fd5099457d24427c3c4e7cfb8df3bedac
Diana <Diana@email.com>
2025-12-04 02:26:53 -0600

    carrito

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css"; // Aquí pondremos los estilos
 
+
 const Home = () => {
   return (
     <div>
@@ -79,7 +80,7 @@ const Home = () => {
             ["🧸", "Osito de Peluche", "Suave y adorable, perfecto para abrazar", "$299"],
             ["🎮", "Consola Portátil", "Diversión en cualquier lugar", "$1,499"],
             ["🧩", "Rompecabezas 3D", "Desafía tu mente y creatividad", "$399"],
-            ["🚗", "Auto a Control Remoto", "Velocidad y diversión garantizada", "$799"],
+            [<img src={require('./../../assets/hot-wheels.jpg')} alt="Hot Wheels" style={{width: '100%', height: '100%', objectFit: 'contain'}} />, "Auto a Control Remoto", "Velocidad y diversión garantizada", "$799"],
             ["🎨", "Set de Arte", "Despierta el artista interior", "$549"],
             ["🎲", "Juego de Mesa", "Diversión para toda la familia", "$449"],
           ].map(([emoji, title, desc, price], i) => (
